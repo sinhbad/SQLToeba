2a1e4b526a64d81a30dd2d298c3af632691346e9
sinhbad <sinhbad@yandex.ru>
2016-05-12 22:54:07 +0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -19,7 +19,3 @@ echo "Cleaning..."
 rm -rf sentences.tar.bz2 links.tar.bz2 sentences.csv links.csv sentencesl.csv sentences_sed.csv
 echo "Ready!"
 echo "Installation completed successfully"
-echo "Enter ./tcv-no-color3 <level> <count> <lc1> <lc2> for showing [count] results from [lc1] to [lc2] with depth [level]"
-echo "For example:"
-echo "./tcv-no-color3 5 100 rus eng"
-echo "./tcv-no-color3 5 100 eng deu"
